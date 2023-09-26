dac63f0561c45f27ab7ea5c88c8ae5b434597838
imartinezurena <nachomartinezurena@gmail.com>
2023-09-26 21:40:53 +0200

    ejercicio

--- PMDM/HolaMundo/app/src/main/java/com/example/holamundo/guessNumber.java
+++ PMDM/HolaMundo/app/src/main/java/com/example/holamundo/guessNumber.java
@@ -3,12 +3,25 @@ package com.example.holamundo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class guessNumber extends AppCompatActivity {
+    Button btnGuess;
+    Button btnRestart;
+    TextView tvClue;
+    EditText ptNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guess_number);
+
+        btnGuess.findViewById(R.id.btIdGuess);
+        btnRestart.findViewById(R.id.btIdRestart);
+        tvClue.findViewById(R.id.TvIdClue);
+        ptNumber.findViewById(R.id.eTIdNumber);
     }
+
 }
