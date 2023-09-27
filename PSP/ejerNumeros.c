9b460b7bc9d18b66265583008b0ffcfb5db1d4cd
imartinezurena <nachomartinezurena@gmail.com>
2023-09-27 18:21:35 +0200

    ejercicios PSP

---
+++ PSP/ejerNumeros.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void){
+float inputFactorial;
+int factorial;
+do{
+    inputFactorial = -1;
+    printf("dame un numero positivo");
+    scanf("%d",&inputFactorial);
+
+}while(inputFactorial<0);
+for(int i=0;i++;i)
+
+    return 0;
+}
---
+++ PSP/ejerPalindromo.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdbool.h>
+#define BUFFER 1024
+int main (void){
+    char inputPalabra[BUFFER];
+    printf("introduce una palabra");
+    scanf("%s",inputPalabra);
+    int longitud =strlen(inputPalabra);
+    bool palindromo=true;
+
+    for(int i=0;i<longitud/2 && palindromo;i++){
+        if(inputPalabra[i]==inputPalabra[longitud-i-1]){
+            continue;
+        }else{
+            palindromo=false;
+        }
+    }
+    if(palindromo){
+        printf("es palindromo");
+    }else{printf("no lo es");}
+    
+
+
+
+
+    return 0;
+}
