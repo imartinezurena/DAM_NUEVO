7c625750281aae0184c9b0d70f5404df57969590
imartinezurena <nachomartinezurena@gmail.com>
2023-10-06 20:12:22 +0200

    AD ejer 6

--- Acceso Datos/Entrega.sql
+++ Acceso Datos/Entrega.sql
@@ -89,6 +89,136 @@ WHERE
     
     END;
 	--ejercicio 3
+	
+	 DROP TABLE temp;
+
+CREATE TABLE temp (
+    cod_emp number,
+    nombre varchar2(30),
+    trabajo varchar2(30)
+);
+ 
+ declare
+ type t_reg_emple  is record(
+ cod_emp employee.employee_id%type,
+ nombre employee.first_name%type,
+ trabajo job.function%type
+ ); 
+ tablaDatos t_reg_emple ;
+ begin
+ select employee.employee_id ,employee.first_name, job.function into tablaDatos
+ from employee, job
+ where employee.job_id=job.job_id and
+ employee.employee_id='7782';
+ insert INTO temp values tabladatos;
+ 
+ end;
+ /
+ --ejercicio 4
+ --ejercicio5
+DROP TABLE temp;
+
+CREATE TABLE temp (
+   contador number(4),
+   nombre varchar2(50)
+);
+DECLARE
+    TYPE nombreclientes IS
+        TABLE OF customer.name%TYPE INDEX BY BINARY_INTEGER;
+    CURSOR c IS
+    SELECT
+        customer.name
+    FROM
+        customer;
+
+    nombre         customer.name%TYPE;
+    contador       BINARY_INTEGER := 1;
+    tablaclientes  nombreclientes;
+BEGIN
+    OPEN c;
+    FETCH c INTO nombre;
+    WHILE c%found LOOP
+        tablaclientes(contador) := nombre;
+        dbms_output.put_line(tablaclientes(contador));
+        insert into temp values (contador, nombre);
+        contador := contador + 1;
+        insert into temp values (contador, nombre);
+        FETCH c INTO nombre;
+    END LOOP;
+
+    CLOSE c;
+END;
+/
+--ejercicio6
+DROP TABLE temploop;
+
+DROP TABLE tempwhile;
+
+DROP TABLE tempfor;
+
+CREATE TABLE temploop (
+    cod_cli  NUMBER(10),
+    nom_cli  VARCHAR2(50),
+    cod_emp  NUMBER(10)
+);
+
+CREATE TABLE tempwhile (
+    cod_cli  NUMBER(10),
+    nom_cli  VARCHAR2(50),
+    cod_emp  NUMBER(10)
+);
+
+CREATE TABLE tempfor (
+    cod_cli  NUMBER(10),
+    nom_cli  VARCHAR2(50),
+    cod_emp  NUMBER(10)
+);
+
+DECLARE
+    CURSOR c IS
+    SELECT
+        customer.customer_id,
+        customer.name,
+        employee.employee_id
+    FROM
+        customer,
+        employee
+    WHERE
+        employee.employee_id = customer.salesperson_id;
+
+    cliente   customer.customer_id%TYPE;
+    nombre    customer.name%TYPE;
+    empleado  employee.employee_id%TYPE;
+BEGIN
+    OPEN c;
+    LOOP
+        FETCH c INTO
+            cliente,
+            nombre,
+            empleado;
+        EXIT WHEN c%notfound;
+        insert into temploop values (cliente, nombre, empleado);
+    END LOOP;
+    CLOSE c;
+    
+    open c;
+    fetch c into cliente, nombre, empleado;
+    while c%found loop
+     insert into tempWhile values (cliente, nombre, empleado);
+    fetch c into cliente, nombre, empleado;
+    end loop;
+    close c;
+    
+    open c;
+    fetch c into cliente, nombre, empleado;
+    for i IN 1..1000 LOOP
+    EXIT WHEN c%notfound;
+    insert into tempFor values (cliente, nombre, empleado);
+    fetch c into cliente, nombre, empleado;
+      END LOOP;
+    close c;
+END;
+/
 
 
 
