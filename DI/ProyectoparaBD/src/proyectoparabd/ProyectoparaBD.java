7f56d5fa96ae720df790bddb093036ecc095a60e
imartinezurena <nachomartinezurena@gmail.com>
2023-10-09 21:38:25 +0200

    base de datos

---
+++ DI/ProyectoparaBD/src/proyectoparabd/ProyectoparaBD.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyectoparabd;
+
+/**
+ *
+ * @author at5DAM2
+ */
+public class ProyectoparaBD {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
