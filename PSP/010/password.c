3bd16981b12ab5917e8d4890b6a601b84a9b1910
imartinezurena <nachomartinezurena@gmail.com>
2023-10-17 21:31:30 +0200

    password.c

--- PSP/010/password.c
+++ PSP/010/password.c
@@ -1,24 +1,57 @@
 #include <stdio.h>
+#include <string.h>
+#include <openssl/evp.h>
+#define MD5_LEN 16
 
-int main (void){
-  int i; //variable contador para el bucle
+void generateMD5(const char *string, unsigned char *str_result)
+{
+  EVP_MD_CTX *ctx;
+  const EVP_MD *md;
+  unsigned char result[EVP_MAX_MD_SIZE];
+
+  ctx = EVP_MD_CTX_new();
+  md = EVP_md5();
+
+  EVP_DigestInit_ex(ctx, md, NULL);
+  EVP_DigestUpdate(ctx, string, strlen(string));
+  EVP_DigestFinal_ex(ctx, result, NULL);
+
+  EVP_MD_CTX_free(ctx);
+
+  for (int i = 0; i < MD5_LEN; i++)
+  { // MD5 result is always 16 bytes
+    sprintf(str_result + (i * 2), "%02x", result[i]);
+  }
+}
+int main(void)
+{
+  int i; // variable contador para el bucle
   int j;
   int a;
   int b;
-    for(i=97; i<123; i++)
+  unsigned char result[EVP_MAX_MD_SIZE];
+  unsigned int result_len;
+  char string[5] = {i, a, b, j, '\0'};
+  for (i = 97; i < 123; i++)
+  {
+    for (a = 97; a < 123; a++)
     {
-      for(a=97; a<123; a++)
+      for (b = 97; b < 123; b++)
       {
-        for(b=97; b<123; b++)
+        for (j = 97; j < 123; j++) // bucle for que recorre los caracteres ASCII
         {
-          for(j=97; j<123; j++)//bucle for que recorre los caracteres ASCII
-          {
-          printf("%c%c%c%c " ,i ,a,b,j);//imprimimos el caracter
-          }
-        }
-      } 
-    }
+          string[0] = i;
+          string[1] = a;
+          string[2] = b;
+          string[3] = j;
+          generateMD5(string, result);
+          
+          printf("%s da %s ", string, result); // imprimimos el caracter
 
-    return 0;
+        }
+      }
+    }
+  }
+
+  return 0;
 }
- 
