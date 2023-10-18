5f945480f83329f225927bbf7536ab602cf6463d
imartinezurena <nachomartinezurena@gmail.com>
2023-10-18 10:29:12 +0200

    md5

--- PSP/010/password.c
+++ PSP/010/password.c
@@ -23,15 +23,26 @@ void generateMD5(const char *string, unsigned char *str_result)
     sprintf(str_result + (i * 2), "%02x", result[i]);
   }
 }
-int main(void)
-{
+int main(void){
   int i; // variable contador para el bucle
   int j;
   int a;
   int b;
+  int d;
   unsigned char result[EVP_MAX_MD_SIZE];
   unsigned int result_len;
+  unsigned char resultado[EVP_MAX_MD_SIZE];
+  unsigned int resultado_len;
   char string[5] = {i, a, b, j, '\0'};
+  char contra[6] = {i, a, b, j, d, '\0'};
+   int auxiliar; //variable contador para el bucle
+  
+ 
+  /*for(auxiliar=0; auxiliar<256; auxiliar++)//bucle for que recorre los 256 caracteres ASCII
+    {
+      printf("%d\t-\t%c\n", auxiliar, auxiliar);//imprimimos el número y el caracter
+    }*/
+
   for (i = 97; i < 123; i++)
   {
     for (a = 97; a < 123; a++)
@@ -45,13 +56,49 @@ int main(void)
           string[2] = b;
           string[3] = j;
           generateMD5(string, result);
-          
-          printf("%s da %s ", string, result); // imprimimos el caracter
-
+          if(strcmp(result,"582fc884d6299814fbd4f12c1f9ae70f")==0){
+          printf("%s da %s \n", string, result); // imprimimos el caracter
+          }
+          else if(strcmp(result,"74437fabd7c8e8fd178ae89acbe446f2")==0){
+          printf("%s da %s \n", string, result); // imprimimos el caracter
+          }
+          else if(strcmp(result,"28ea19352381b8659df830dd6d5c90a3")==0){
+          printf("%s da %s \n", string, result); // imprimimos el caracter
+          }
+          else if(strcmp(result,"90f077d7759d0d4d21e6867727d4b2bd")==0){
+          printf("%s da %s \n", string, result); // imprimimos el caracter
+          }
         }
       }
     }
   }
+  for (i = 65; i < 123; i++)
+  {
+    for (a = 65; a < 123; a++)
+    {
+      for (b = 65; b < 123; b++)
+      {
+        for (j = 65; j < 123; j++) // bucle for que recorre los caracteres ASCII
+        {
+          for (d = 65; d < 123; d++) // bucle for que recorre los caracteres ASCII
+        {
+          contra[0] = i;
+          contra[1] = a;
+          contra[2] = b;
+          contra[3] = j;
+          contra[4] = d;
+          generateMD5(contra, resultado);
+          if(strcmp(resultado,"f4a1c8901a3d406f17af67144a3ec71a")==0){
+          printf("%s da %s \n", contra, result); // imprimimos el caracter
+          }
+          else if(strcmp(resultado,"d66e29062829e8ae0313adc5a673f863")==0){
+          printf("%s da %s \n", contra, result); // imprimimos el caracter
+          }
+        }
+      }
+    }
+  }
+}  
 
   return 0;
 }
