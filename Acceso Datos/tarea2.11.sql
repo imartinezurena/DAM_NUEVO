4c45a3380f7d64590b01e4fdd700fb3aa5a83eb6
imartinezurena <nachomartinezurena@gmail.com>
2023-11-30 18:02:42 +0100

    ad tarea 11

---
+++ Acceso Datos/tarea2.11.sql
@@ -0,0 +1,12 @@
+--ejercicio 1
+create or replace type Tipo_direccion as object (
+direccion varchar(50),
+codigo_postal varchar(20)
+);
+--ejercicio 4
+ describe tipo_descripciones;
+ describe tipo_contacto;
+ describe tipo_persona;
+ describe tipo_cliente;
+ 
+
