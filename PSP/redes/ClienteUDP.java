9ef1c6695b5533e9df36ce8051fee1fef3df46b2
vm portatil <nachomartinezurena@gmail.com>
2023-12-13 17:55:03 +0100

    redes psp

---
+++ PSP/redes/ClienteUDP.java
@@ -0,0 +1,23 @@
+package redes;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+public class ClienteUDP {
+    public static void main(String[] args) {
+        try {
+            DatagramSocket socket = new DatagramSocket();
+            InetAddress IPAddress = InetAddress.getByName("localhost"); // Dirección del servidor
+            byte[] sendData = new byte[1024];
+            String sentence = "facundo mamaguevo"; // Mensaje a enviar
+            sendData = sentence.getBytes();
+
+            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
+            socket.send(sendPacket); // Envía el paquete al servidor
+            socket.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
