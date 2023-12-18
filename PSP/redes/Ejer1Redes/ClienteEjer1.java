c6ca0236c39f64fce141a71246b3b964cfd19690
imartinezurena <nachomartinezurena@gmail.com>
2023-12-19 00:21:11 +0100

    redes

--- PSP/redes/Ejer1Redes/ClienteEjer1.java
+++ PSP/redes/Ejer1Redes/ClienteEjer1.java
@@ -1,5 +1,49 @@
-//Crea un programa cliente que reciba por parámetro
- //una cadena representando la dirección ip,
- //un número representando el puerto y una última cadena
-  //representando el texto a enviar. 
-  //El programa enviará la información usando UDP.
+/*
+ * Crea un programa cliente que reciba por parámetro
+ * una cadena representando la dirección ip,
+ * un número representando el puerto y una última cadena
+ * representando el texto a enviar.
+ * El programa enviará la información usando UDP.
+ */
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+
+public class ClienteEjer1 {
+
+    public static void main(String[] args) {
+        // ClienteEjer1 cliente = new ClienteEjer1(args[0], Integer.valueOf(args[1]),
+        // args[2]);
+        String direccionIP = args[0];
+        String texto = args[1];
+        int puerto = Integer.valueOf(args[2]);
+        try {
+            DatagramSocket socket = new DatagramSocket();
+            InetAddress IPAddress = InetAddress.getByName(direccionIP); // Dirección del servidor
+            byte[] sendData = new byte[1024];
+            // Mensaje a enviar
+            sendData = texto.getBytes();
+            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, puerto);
+            socket.send(sendPacket); // Envía el paquete al servidor
+            socket.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /*
+     * public ClienteEjer1(String direccionIP, int puerto, String texto) {
+     * this.direccionIP = direccionIP;
+     * this.puerto = puerto;
+     * this.texto = texto;
+     * try {
+     * DatagramSocket socket = new DatagramSocket(puerto);
+     * } catch (SocketException e) {
+     * // TODO Auto-generated catch block
+     * e.printStackTrace();
+     * }
+     * }
+     */
+}
--- PSP/redes/Ejer1Redes/ServidorEjer1.java
+++ PSP/redes/Ejer1Redes/ServidorEjer1.java
@@ -1,3 +1,27 @@
 //Crea un programa servidor que reciba por parámetro
 //un número que representa el puerto en el que escuchará,
 //cuando reciba un mensaje lo escribirá por pantalla.
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
+public class ServidorEjer1 {
+    // poner e ip como inet adress y ejecutar cliente con esa
+    public static void main(String[] args) {
+        try {
+            DatagramSocket socket = new DatagramSocket(9877, "192.168.73.223"); // Abre el socket en el puerto 9877
+            byte[] receivedData = new byte[1024];
+
+            while (true) {
+                DatagramPacket receivedPacket = new DatagramPacket(receivedData, receivedData.length);
+                socket.receive(receivedPacket); // Espera y recibe el paquete
+
+                // Extrae la información del paquete
+                String message = new String(receivedPacket.getData(), 0, receivedPacket.getLength());
+                System.out.println("Mensaje recibido: " + message);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
