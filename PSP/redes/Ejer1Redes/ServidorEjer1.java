2f52c04d76236043ada0650245a010ab5e6ad832
imartinezurena <nachomartinezurena@gmail.com>
2023-12-19 16:19:25 +0100

    REDES EJR 1

--- PSP/redes/Ejer1Redes/ServidorEjer1.java
+++ PSP/redes/Ejer1Redes/ServidorEjer1.java
@@ -4,18 +4,21 @@
 
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.Inet4Address;
+import java.net.InetAddress;
 
 public class ServidorEjer1 {
     // poner e ip como inet adress y ejecutar cliente con esa
     public static void main(String[] args) {
+        InetAddress direccion;
         try {
-            DatagramSocket socket = new DatagramSocket(9877, "192.168.73.223"); // Abre el socket en el puerto 9877
+            DatagramSocket socket = new DatagramSocket(9877); // Abre el socket en el puerto 9877
             byte[] receivedData = new byte[1024];
 
             while (true) {
                 DatagramPacket receivedPacket = new DatagramPacket(receivedData, receivedData.length);
                 socket.receive(receivedPacket); // Espera y recibe el paquete
-
+                direccion = receivedPacket.getAddress();
                 // Extrae la información del paquete
                 String message = new String(receivedPacket.getData(), 0, receivedPacket.getLength());
                 System.out.println("Mensaje recibido: " + message);
