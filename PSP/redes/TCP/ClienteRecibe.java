0b3583f927d35997d4d2dcd8108004ff4deb3d41
Imartinezurena <nachomartinezurena@gmail.com>
2024-02-12 17:14:37 +0100

    sacar directorio podria ser peor

---
+++ PSP/redes/TCP/ClienteRecibe.java
@@ -0,0 +1,21 @@
+package redes.TCP;
+
+import java.net.Socket;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class ClienteRecibe {
+    public static void main(String[] args) {
+        try {
+            Socket socket = new Socket("127.0.0.1", 1234);
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            System.out.println(in.readLine());
+            in.close();
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
